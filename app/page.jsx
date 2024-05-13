27c8968832ace32c29a6bf9f1c1b23ef007f047b
Eftay Aksular <95072387+eftay17@users.noreply.github.com>
2024-05-13 09:08:20 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react';
-import { Input } from '@nextui-org/input';
-import { Button } from '@nextui-org/button';
+'use client';
 import { SearchIcon } from '@/components/icons';
 import StakeTable from '@/components/table';
+import {
+  baseUrl,
+  myStakesQuery,
+  receivedStakesQuery,
+  rues,
+} from '@/config/site';
+import { Button } from '@nextui-org/button';
+import { Input } from '@nextui-org/input';
 import axios from 'axios';
+import { useState } from 'react';
 import { Web3 } from 'web3';
 import { isAddress } from 'web3-validator';
-import { baseUrl, myStakesQuery, receivedStakesQuery, rues } from '@/config/site';
 
-const MyComponent = () => {
+const myStakes = new Map();
+const receivedStakes = new Map();
+
+export default function Home() {
   const [wallet, setWallet] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [unreturnedStakes, setUnreturnedStakes] = useState([]);
@@ -24,66 +33,129 @@ const MyComponent = () => {
       return;
     }
 
+    myStakes.clear();
+    receivedStakes.clear();
+    setUnreturnedStakes([]);
     setIsLoading(true);
+
     try {
-      const res1 = await axios.post(baseUrl, {
-        operationName: 'GetStakesSent',
-        query: myStakesQuery,
-        variables: { address: wallet },
-      });
+      await axios
+        .post(baseUrl, {
+          operationName: 'GetStakesSent',
+          query: myStakesQuery,
+          variables: {
+            address: wallet,
+          },
+        })
+        .then((res) => {
+          res.data.data.stakes.forEach((stake) => {
+            const amount = Number.parseFloat(
+              Web3.utils.fromWei(stake.amount, 'ether')
+            );
+            const staker = stake.candidate.id;
+            if (staker === rues && amount >= 1) {
+              return;
+            }
+            myStakes.set(staker, amount);
+          });
+        });
 
-      const myStakes = res1.data.data.stakes.map((stake) => ({
-        amount: Number.parseFloat(Web3.utils.fromWei(stake.amount, 'ether')),
-        staker: stake.candidate.id,
-      }));
+      await axios
+        .post(baseUrl, {
+          operationName: 'GetStakesSent',
+          query: receivedStakesQuery,
+          variables: {
+            address: wallet,
+          },
+        })
+        .then((res) => {
+          res.data.data.stakes.forEach((stake) => {
+            const amount = Number.parseFloat(
+              Web3.utils.fromWei(stake.amount, 'ether')
+            );
+            const staker = stake.staker.id;
+            receivedStakes.set(staker, amount);
+          });
+        });
 
-      const res2 = await axios.post(baseUrl, {
-        operationName: 'GetStakesReceived',
-        query: receivedStakesQuery,
-        variables: { address: wallet },
-      });
+      const unreturned = [];
 
-      const receivedStakes = res2.data.data.stakes.map((stake) => ({
-        amount: Number.parseFloat(Web3.utils.fromWei(stake.amount, 'ether')),
-        staker: stake.staker.id,
-      }));
+      for (let [staker, amount] of myStakes.entries()) {
+        if (amount == 0) continue;
 
-      const unreturned = myStakes.filter((myStake) => {
-        const receivedStake = receivedStakes.find((r) => r.staker === myStake.staker);
-        return !receivedStake || myStake.amount > receivedStake.amount;
-      });
+        const receivedStakedAmount = receivedStakes.get(staker);
+        if (
+          receivedStakedAmount === undefined ||
+          amount <> receivedStakedAmount
+        ) {
+          unreturned.push({
+            staker: staker,
+            youStaked: amount,
+            receivedStake: receivedStakedAmount || 0,
+          });
+        }
+      }
+
+      // const pending = [];
+      // for (let [staker, amount] of receivedStakes.entries()) {
+      //   if (amount == 0) continue;
+
+      //   const myStakedAmount = myStakes.get(staker);
+      //   if (myStakedAmount === undefined || myStakedAmount < amount) {
+      //     pending.push({
+      //       staker: staker,
+      //       youStaked: myStakedAmount || 0,
+      //       receivedStake: amount,
+      //     });
+      //   }
+      // }
 
       setUnreturnedStakes(unreturned);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.log(error);
     } finally {
       setIsLoading(false);
     }
   };
 
   return (
-    <div className="container">
-      <Input
-        placeholder="Enter your wallet address"
-        errorMessage="Please enter a valid address"
-        isInvalid={isInvalid}
-        endContent={
-          <Button isIconOnly onClick={sendQueries}>
-            <SearchIcon />
-          </Button>
-        }
-        onChange={(e) => {
-          const address = e.target.value.trim();
-          if (address === '') {
-            setIsInvalid(false);
-            return;
+    <div className="w-full flex flex-col justify-center items-start gap-10">
+      <div className="flex w-full items-center justify-center gap-2">
+        <Input
+          isInvalid={isInvalid}
+          placeholder="Enter your wallet address"
+          errorMessage="Please enter valid address"
+          endContent={
+            <Button
+              className="bg-transparent hover:bg-transparent"
+              isIconOnly
+              onClick={sendQueries}
+            >
+              <SearchIcon />
+            </Button>
           }
-          setWallet(address);
-        }}
+          onChange={(e) => {
+            const address = e.target.value.trim();
+            if (address === '') {
+              setIsInvalid(false);
+              return;
+            }
+            setWallet(address);
+          }}
+        ></Input>
+      </div>
+      <div className="w-full flex items-center justify-center text-amber-300">
+        <p className="text-sans text-sm">
+          <span className="font-bold text-amber-600">Attention:</span> This
+          table only shows the adresses that does not mutually stake with you.
+        </p>
+      </div>
+
+      <StakeTable
+        isLoading={isLoading}
+        items={unreturnedStakes}
+        sendQueries={sendQueries}
       />
-      <StakeTable isLoading={isLoading} items={unreturnedStakes} />
     </div>
   );
-};
-
-export default MyComponent;
+}
